237470682df0d7b308be346424d91582d2fda5bf
Chris.Muiru <chrismuiru8@gmail.com>
2024-01-26 11:45:23 +0300

    initial commit

---
+++ src/test/java/com/muiru/sheets/SheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muiru.sheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
